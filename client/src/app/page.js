f082a34decf2f47db1cd0362e56d5ccf1ab70343
@benjamin lucky <bamidelebenjamin5@gmail.com>
2025-05-24 15:55:51 +0100

    featured candidates added

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -8,6 +8,8 @@ import clsx from "clsx";
 import { featuredJobs } from "../../data";
 import { IoLocationOutline } from "react-icons/io5";
 import { FiBriefcase } from "react-icons/fi";
+import FeaturedJobs from "@/components/FeaturedJobs";
+import FeaturedCandidates from "@/components/FeaturedCandidates";
 
 const rotatingWords = ["Find Jobs", "Post Job", "Find Candidates"];
 
@@ -106,78 +108,8 @@ export default function HomePage() {
         </div>
 
         {/* Featured Jobs Infinite Scroll */}
-        <div className="featuredJobs mt-12 overflow-hidden">
-          <h2 className="text-4xl font-bold text-center text-gray-300 my-8">
-            Featured Jobs
-          </h2>
-
-          {/* Infinite Scrolling Container */}
-          <div className="relative w-full overflow-hidden">
-            <motion.div
-              className="flex gap-4 w-max"
-              animate={controls}
-              onMouseEnter={() => controls.stop()}
-              onMouseLeave={() =>
-                controls.start({
-                  x: ["0%", "-100%"],
-                  transition: {
-                    repeat: Infinity,
-                    duration: 60,
-                    ease: "linear",
-                  },
-                })
-              }
-            >
-              {[...featuredJobs, ...featuredJobs].map((job, idx) => (
-                <div
-                  key={idx}
-                  className="min-w-[300px] max-w-xs w-full p-6 rounded-lg bg-white shadow-md flex-shrink-0"
-                >
-                  <div className="flex items-center justify-between mb-4">
-                    <div>
-                      <h3 className="text-lg font-bold text-gray-800">
-                        {job.title}
-                      </h3>
-                      <p className="text-sm text-gray-600">{job.company}</p>
-                    </div>
-                    <img
-                      src={job.companyLogo}
-                      alt={job.company}
-                      className="w-12 h-12 object-contain"
-                    />
-                  </div>
-
-                  <p className="text-gray-600 text-sm mb-4 break-words line-clamp-3">
-                    {job.brief}
-                  </p>
-
-                  <div className="flex items-center justify-between text-sm text-gray-600 mb-4">
-                    <div className="flex items-center gap-1">
-                      <IoLocationOutline className="text-lime-500" />
-                      <span>{job.location}</span>
-                    </div>
-                    <div className="flex items-center gap-1">
-                      <FiBriefcase className="text-lime-500" />
-                      <span>{job.mode}</span>
-                    </div>
-                  </div>
-
-                  <div className="flex items-center justify-between mt-6">
-                    <div>
-                      <p className="text-gray-800 font-semibold">
-                        {job.salary}
-                      </p>
-                      <p className="text-gray-500 text-xs">{job.time}</p>
-                    </div>
-                    <button className="px-3 py-2 bg-lime-500 text-white text-sm font-medium rounded hover:bg-lime-700 transition">
-                      Apply Now
-                    </button>
-                  </div>
-                </div>
-              ))}
-            </motion.div>
-          </div>
-        </div>
+        <FeaturedJobs />
+        <FeaturedCandidates />
       </div>
     </main>
   );
---
+++ client/src/components/FeaturedCandidates.jsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+import { featuredCandidates, featuredJobs } from "../../data";
+import { IoLocationOutline } from "react-icons/io5";
+import { FiBriefcase } from "react-icons/fi";
+
+export default function FeaturedCandidates() {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start({
+      x: ["0%", "-100%"],
+      transition: {
+        repeat: Infinity,
+        duration: 60,
+        ease: "linear",
+      },
+    });
+  }, [controls]);
+
+  return (
+    <div className="featuredJobs mt-12 overflow-hidden">
+      <h2 className="text-4xl font-bold text-center text-gray-300 my-8">
+        Featured Candidates
+      </h2>
+
+      <div className="relative w-full overflow-hidden">
+        <motion.div
+          className="flex gap-4 w-max"
+          animate={controls}
+          onMouseEnter={() => controls.stop()}
+          onMouseLeave={() =>
+            controls.start({
+              x: ["0%", "-100%"],
+              transition: {
+                repeat: Infinity,
+                duration: 60,
+                ease: "linear",
+              },
+            })
+          }
+        >
+          {[...featuredCandidates, ...featuredCandidates].map(
+            (candidate, idx) => (
+              <div
+                key={idx}
+                className="min-w-[300px] max-w-xs w-full p-6 rounded-lg bg-black shadow-md flex-shrink-0 hover:shadow-lg transition duration-300"
+              >
+                <div className="flex items-center justify-between mb-4">
+                  <div>
+                    <h3 className="text-lg font-bold text-white">
+                      {candidate.role}
+                    </h3>
+                    <p className=" text-primary-500 font-semibold text-lg">
+                      {candidate.name}
+                    </p>
+                  </div>
+                  <img
+                    src={candidate.avatar}
+                    alt={candidate.name}
+                    className="w-14 h-14 rounded-full object-cover"
+                  />
+                </div>
+
+                <p className="text-gray-600 text-sm mb-4 break-words line-clamp-3">
+                  {candidate.brief}
+                </p>
+
+                <div className="flex items-center justify-between text-sm text-gray-600 mb-4">
+                  <div className="flex items-center gap-1">
+                    <IoLocationOutline className="text-lime-500" />
+                    <span className="text-sm text-white">
+                      {candidate.qualification}
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <FiBriefcase className="text-lime-500" />
+                    <span className="text-sm text-gray-50">
+                      {candidate.location}
+                    </span>
+                  </div>
+                </div>
+
+                <div className="flex w-full items-center  mt-6">
+                  <button className="w-full px-3 py-2 border border-lime-500 text-white text-sm font-medium rounded-full hover:bg-lime-700 transition">
+                    Apply Now
+                  </button>
+                </div>
+              </div>
+            )
+          )}
+        </motion.div>
+      </div>
+    </div>
+  );
+}
