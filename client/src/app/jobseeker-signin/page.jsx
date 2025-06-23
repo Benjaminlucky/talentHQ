5d10fc46cf1c828af73890ba7b123a375f9effd9
@benjamin lucky <bamidelebenjamin5@gmail.com>
2025-06-23 18:02:59 +0100

    env file adjusted

--- client/src/app/jobseeker-signin/page.jsx
+++ client/src/app/jobseeker-signin/page.jsx
@@ -28,7 +28,7 @@ export default function Page() {
 
     try {
       const response = await fetch(
-        "http://localhost:5000/api/jobseekers/login",
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/jobseekers/login`,
         {
           method: "POST",
           headers: {
