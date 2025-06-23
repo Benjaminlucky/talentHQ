960788cf69165379afccb80a3b2fa3c93da5f886
@benjamin lucky <bamidelebenjamin5@gmail.com>
2025-06-23 19:31:56 +0100

    signin handle submit updated

--- client/src/app/jobseeker-signin/page.jsx
+++ client/src/app/jobseeker-signin/page.jsx
@@ -28,7 +28,7 @@ export default function Page() {
 
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/jobseekers/login`,
+        `${process.env.NEXT_PUBLIC_API_BASE}/api/jobseekers/login`,
         {
           method: "POST",
           headers: {
